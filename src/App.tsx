3fda0e09ba5ea1924f41dbcdf083f521ba407070
Dima <dpolevodin@gmail.com>
2022-02-05 00:21:52 +0500

    added router and basic folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      Ведомость школы JS
+      <Routes>
+        <Route path="/" element={<div>Ведомость школы JS</div>} />
+        <Route path="/login" element={<div>login</div>} />
+        <Route path="/admin-login" element={<div>admin-login</div>} />
+        <Route path="/student" element={<div>student</div>} />
+        <Route path="/admin" element={<div>admin</div>} />
+      </Routes>
     </>
   );
 }
